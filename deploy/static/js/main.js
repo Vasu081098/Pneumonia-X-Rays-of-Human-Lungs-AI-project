536197c55bf3ff8d553ea7a9b6287cf54f81942d
Vasu081098 <62716777+Vasu081098@users.noreply.github.com>
2020-08-27 16:31:11 +0530

    Update main.js

--- deploy/static/js/main.js
+++ deploy/static/js/main.js
@@ -45,7 +45,7 @@ $(document).ready(function () {
                 // Get and display the result
                 $('.loader').hide();
                 $('#result').fadeIn(600);
-                $('#result').text(' Diagnosis of lungs status:  ' + data);
+                $('#result').text(' Result:  ' + data);
                 console.log('Success!');
             },
         });
